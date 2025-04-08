6f2f1a55f232160f445450e338cf6d3d544d4793
Sedna <seventesprit@gmail.com>
2025-04-08 21:38:44 +0200

    Map Prototype v2
    1. movement to adjacent tile on mouse click
    2. switching tilemaps on spacebar
    3. code organisation
    
    to do:
    analyse possible script separation

--- Assets/Scenes/World Scene/MovementScript.cs
+++ Assets/Scenes/World Scene/MovementScript.cs
@@ -8,31 +8,14 @@ using UnityEngine.UIElements;
 
 public class MovementScript : MonoBehaviour
 {
-    public Tilemap tilemap;
+    Tilemap tilemap;
     public float hexSize = 1f;
-
-    Vector3Int[] adjacent_even = new Vector3Int[] {
-        new Vector3Int(1, 0, 0),    // North
-        new Vector3Int(-1, 0, 0),   // South
-        new Vector3Int(0, -1, 0),   // N-West
-        new Vector3Int(0, 1, 0),    // N-East
-        new Vector3Int(-1, -1, 0),   // S-West
-        new Vector3Int(-1, 1, 0)   // S-East
-    };
-
-    Vector3Int[] adjacent_odd = new Vector3Int[] {
-        new Vector3Int(1, 0, 0),    // North
-        new Vector3Int(-1, 0, 0),   // South
-        new Vector3Int(1, -1, 0),   // N-West
-        new Vector3Int(1, 1, 0),    // N-East
-        new Vector3Int(0, -1, 0),   // S-West
-        new Vector3Int(0, 1, 0)   // S-East
-    };
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = tilemap.CellToWorld(new Vector3Int(1,1,0));
+        SwitchMap();
+        transform.position = tilemap.CellToWorld(new Vector3Int(0,0,0));
     }
 
     // Update is called once per frame
@@ -41,6 +24,10 @@ public class MovementScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             HandleInput();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchMap();
         }
     }
 
@@ -52,7 +39,10 @@ public class MovementScript : MonoBehaviour
 
         if (tilemap.HasTile(clickCell))
         {
-            Move(clickCell);
+            if (CheckAdjacency(clickCell))
+            {
+                Move(clickCell);
+            }
         }
         else
         {
@@ -62,6 +52,36 @@ public class MovementScript : MonoBehaviour
 
     void Move(Vector3Int targetCell)
     {
+        transform.position = tilemap.GetCellCenterWorld(targetCell);
+    }
+
+
+    ///
+    /// ADJACENCY ON HEXAGONAL GRID
+    /// 
+    Vector3Int[] adjacent_even = new Vector3Int[] {
+        new Vector3Int(1, 0, 0),    // North
+        new Vector3Int(-1, 0, 0),   // South
+        new Vector3Int(0, -1, 0),   // N-West
+        new Vector3Int(0, 1, 0),    // N-East
+        new Vector3Int(-1, -1, 0),   // S-West
+        new Vector3Int(-1, 1, 0)   // S-East
+    };
+    Vector3Int[] adjacent_odd = new Vector3Int[] {
+        new Vector3Int(1, 0, 0),    // North
+        new Vector3Int(-1, 0, 0),   // South
+        new Vector3Int(1, -1, 0),   // N-West
+        new Vector3Int(1, 1, 0),    // N-East
+        new Vector3Int(0, -1, 0),   // S-West
+        new Vector3Int(0, 1, 0)   // S-East
+    };
+    /*
+     * unity has (x,y) hexagonal coordinates where x values grow to the right, and y values grow upwards
+     * that brings issues on diagonals, as x axis would change every 2 steps
+     * that's why we have different adjacency definition on even and odd cells
+     */
+    bool CheckAdjacency(Vector3Int targetCell)
+    {
         Vector3Int playerCell = tilemap.WorldToCell(transform.position);
 
         foreach (Vector3Int direction in (playerCell.y % 2 == 0 ? adjacent_even : adjacent_odd))
@@ -70,8 +90,31 @@ public class MovementScript : MonoBehaviour
 
             if (targetCell == adjacentCell)
             {
-                Debug.Log("Clicked tile is adjacent!");
+                return true;
             }
         }
+        return false;
     }
+    ///
+
+    ///
+    /// MAP SWITCHING GENERAL LOGIC
+    ///
+    public GameObject[] tilemapPrefabs;
+    public GameObject currentMap;
+    int map = 0;
+    /*
+     * just store the prefabs in an array and randomize
+     * could be used for map generation by offsets and matching prefabs for different directions
+     * since the prefab is whole grid, multiple tilemap layers can be used for map objects
+     */
+    void SwitchMap()
+    {
+        if (currentMap != null)
+            Destroy(currentMap);
+        currentMap = Instantiate(tilemapPrefabs[map], Vector3.zero, Quaternion.identity);
+        tilemap = currentMap.GetComponentInChildren<Tilemap>();
+        map = (map + 1) % (tilemapPrefabs.Length);
+    }
+    ///
 }
